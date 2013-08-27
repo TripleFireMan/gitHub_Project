9357885d8f15efd656d766023986dec8e2bd8f10
Mrs.right <ab364743113@126.com>
2013-08-27 14:50:07 +0800

    Initial Commit

---
+++ gitHub_Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitHub_Project
+//
+//  Created by CY on 13-8-27.
+//  Copyright (c) 2013年 chengYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
